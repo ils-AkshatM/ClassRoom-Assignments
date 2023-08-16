e92106cf7603fd3a3ed77ba5f9d49a09d81fecf9
AkshatM17 <a.maheshwari@thesynapses.com>
2023-08-16 11:52:53 +0530

    Created a Counter Application with Redux

---
+++ redux-implementaion/src/App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { actions } from './store/index';
+
+function App() {
+  const counter = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
+  const increment = () => {
+    dispatch(actions.increment());
+  };
+
+  const decrement = () => {
+    dispatch(actions.decrement());
+  };
+
+  const addBy = () => {
+    dispatch(actions.addBy(10));
+  }
+  return (
+    <div>
+      <h1>Counter</h1>
+      <h2>{counter}</h2>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+      <button onClick={addBy}>Add By 10</button>
+
+
+    </div>
+  );
+}
+
+export default App;
